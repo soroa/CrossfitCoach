56853c84df94328d6f326d5387d9ac0ded1326f5
soroa <soroa@student.ethz.ch>
2018-05-01 12:38:31 +0200

    Add SensorPlacement Activity

---
+++ app/src/main/java/com/example/mac/crossfitcoach/screens/sensor_placement/SensorPlacementActivity.kt
@@ -0,0 +1,31 @@
+package com.example.mac.crossfitcoach.screens.sensor_placement
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.wearable.activity.WearableActivity
+import com.example.mac.crossfitcoach.R
+import com.example.mac.crossfitcoach.dbjava.SensorReading
+import com.example.mac.crossfitcoach.screens.main.MainMenuActivity
+import com.example.mac.crossfitcoach.utils.SharedPreferencesHelper
+import kotlinx.android.synthetic.main.activity_sensor_placement.*
+
+class SensorPlacementActivity : WearableActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sensor_placement)
+
+        val sharedPreferencesHelper = SharedPreferencesHelper(this)
+        sharedPreferencesHelper.setIsFirstTime(false)
+        wrist.setOnClickListener {
+            sharedPreferencesHelper.setSmartwatchPosition(SensorReading.WRIST)
+            val i = Intent(this, MainMenuActivity::class.java)
+            startActivity(i)
+        }
+        ankle.setOnClickListener {
+            sharedPreferencesHelper.setSmartwatchPosition(SensorReading.ANKLE)
+            val i = Intent(this, MainMenuActivity::class.java)
+            startActivity(i)
+        }
+    }
+}
